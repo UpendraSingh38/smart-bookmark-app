239b0c8227079b4126fe9b8d8a82515df2bdb426
Upendra Singh <upendrasingh9989@gmail.com>
2026-02-19 00:38:38 +0530

    Update BookmarkForm.js

--- components/BookmarkForm.js
+++ components/BookmarkForm.js
@@ -6,7 +6,7 @@ import { createBrowserClient } from "@supabase/ssr"
 export default function BookmarkForm() {
   const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_KEY
   )
 
   const [title, setTitle] = useState("")
